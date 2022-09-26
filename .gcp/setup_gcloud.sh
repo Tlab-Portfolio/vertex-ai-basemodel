5e94e5d274dfe05d9d9ea5c9e9bc41922044e0cf
td <tlab241@gmail.com>
2022-09-26 06:22:59 +0000

    fix: load env in the script

--- .gcp/setup_gcloud.sh
+++ .gcp/setup_gcloud.sh
@@ -1,6 +1,11 @@
-# 機密情報を使ったサービスアカウントの有効化
-gcloud auth activate-service-account --key-file .gcp/GCP_credential.json
+#! /usr/bin/env bash
+
+# 環境変数を読み込む
+source .env
 
 # gcloudコマンドの設定
 gcloud config set project $PROJECT_ID
 gcloud config set compute/region $REGION
+
+# 機密情報を使ったサービスアカウントの有効化
+# gcloud auth activate-service-account --key-file .gcp/GCP_credential.json
