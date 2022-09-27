277355fd4abc1edc5070c2c29ef27f241863439e
td <tlab241@gmail.com>
2022-09-27 09:08:14 +0000

    fix: to deploy by deployment configs

--- .gcp/setup_gcloud.sh
+++ .gcp/setup_gcloud.sh
@@ -3,9 +3,12 @@
 # 環境変数を読み込む
 source .env
 
-# gcloudコマンドの設定
+# ローカルにおけるgcloudコマンドの設定
 gcloud config set project $PROJECT_ID
 gcloud config set compute/region $REGION
+
+# Deployment Manger APIを有効にする
+gcloud services enable deploymentmanager.googleapis.com
 
 # 機密情報を使ったサービスアカウントの有効化
 # gcloud auth activate-service-account --key-file .gcp/GCP_credential.json
