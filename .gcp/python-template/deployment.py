e3ea07bb1ff11223f20089f146a7ce268bc4dc62
td <tlab241@gmail.com>
2022-09-27 02:05:37 +0000

    add&refact: python template for deployment

---
+++ .gcp/python-template/deployment.py
@@ -0,0 +1,155 @@
+# NOTE: .envから環境変数を読み込もうとしたが、エラーとなった。
+# Deployment ManagerのPythonテンプレートでは特定のパッケージ以外は特別な方法でインポートする必要があるみたい。
+# import importlib.util as util
+# import sys
+
+# spec = util.spec_from_file_location("os", "/usr/local/lib/python3.10/os.py")
+# os = util.module_from_spec(spec)
+# sys.modules["os"] = os
+# spec.loader.exec_module(os)
+
+# dotenv_path = "/usr/local/lib/python3.10/site-packages/dotenv/__init__.py"
+# spec = util.spec_from_file_location("dotenv", dotenv_path)
+# dotenv = util.module_from_spec(spec)
+# sys.modules["dotenv"] = dotenv
+# spec.loader.exec_module(dotenv)
+
+
+# load_dotenv(verbose=True)
+# pj_root_path = Path(__file__).parent.parent
+# dotenv_path = pj_root_path / ".env"
+# load_dotenv(dotenv_path)
+
+
+def GenerateConfig(context):
+    """Generate configuration."""
+
+    # General setting
+    PROJECT_ID = context.env["project"]
+    ZONE = context.properties["zone"]
+
+    # Service Account
+    SERVICE_ACCOUNT_ID = context.properties["serviceAccountId"]
+    SERVICE_ACCOUNT_DISPLAYNAME = context.properties["serviceAccountDisplayName"]
+    SERVICE_ACCOUNT = f"{SERVICE_ACCOUNT_ID}@{PROJECT_ID}.iam.gserviceaccount.com"
+
+    # Storage
+    # BUCKET_NAME = os.environ.get("BUCKET_NAME")
+    BUCKET_NAME = "bucket_" + context.env["project"]
+    DATASET_NAME = context.properties["datasetName"]
+    TABLE_NAME = context.properties["tableName"]
+
+    resources = []
+
+    # Enable Service APIs
+    resources.append(
+        {
+            "type": "deploymentmanager.v2.virtual.enableService",
+            "name": "enable-compute-api",
+            "properties": {
+                "consumerId": f"project: {PROJECT_ID}",
+                "serviceName": "compute.googleapis.com",
+            },
+        }
+    )
+    resources.append(
+        {
+            "type": "deploymentmanager.v2.virtual.enableService",
+            "name": "enable-storage-api",
+            "properties": {
+                "consumerId": f"project: {PROJECT_ID}",
+                "serviceName": "storage.googleapis.com",
+            },
+        }
+    )
+    resources.append(
+        {
+            "type": "deploymentmanager.v2.virtual.enableService",
+            "name": "enable-bigquery-api",
+            "properties": {
+                "consumerId": f"project: {PROJECT_ID}",
+                "serviceName": "bigquery.googleapis.com",
+            },
+        }
+    )
+    resources.append(
+        {
+            "type": "deploymentmanager.v2.virtual.enableService",
+            "name": "enable-aiplatform-api",
+            "properties": {
+                "consumerId": f"project: {PROJECT_ID}",
+                "serviceName": "aiplatform.googleapis.com",
+            },
+        }
+    )
+
+    # Creat a serivece account
+    resources.append(
+        {
+            "type": "iam.v1.serviceAccount",
+            "name": "test-account",
+            "properties": {
+                "accountId": SERVICE_ACCOUNT_ID,
+                "displayName": SERVICE_ACCOUNT_DISPLAYNAME,
+                "projectId": PROJECT_ID,
+            },
+        }
+    )
+    resources.append(
+        {
+            "type": "gcp-types/cloudresourcemanager-v1:virtual.projects.iamMemberBinding",
+            "name": "bind-iam-policy",
+            "properties": {
+                "resource": PROJECT_ID,
+                "role": "roles/editor",
+                # NOTE: "serviceAccount:value" 全体を文字列としないとエラーとなる
+                "member": f"serviceAccount:{SERVICE_ACCOUNT}",
+            },
+            "metadata": {"dependsOn": ["test-account"]},
+        }
+    )
+
+    # Create a storage bucket
+    resources.append(
+        {
+            "type": "storage.v1.bucket",
+            "name": BUCKET_NAME,  # Deployment name and bucket name
+            "properties": {
+                "resource": PROJECT_ID,
+                "location": ZONE,
+                "predefinedAcl": "projectPrivate",
+                "projection": "full",
+                "storageClass": "STANDARD",
+            },
+        }
+    )
+
+    # Create a dataset on BigQuery
+    resources.append(
+        {
+            "type": "bigquery.v2.dataset",
+            "name": "demo_dataset",
+            "properties": {
+                "datasetReference": {
+                    "datasetId": DATASET_NAME,  # Actual dataset name
+                },
+                "location": ZONE,
+                "description": "dataset for kubeflow demo",
+            },
+        }
+    )
+    resources.append(
+        {
+            "type": "bigquery.v2.table",
+            "name": "demo_table",
+            "properties": {
+                "datasetId": DATASET_NAME,  # Actual dataset name
+                "tableReference": {
+                    "tableId": TABLE_NAME,  # Actual dataset name
+                },
+            },
+            "metadata": {"dependsOn": ["demo_dataset"]},
+        }
+    )
+
+    return {"resources": resources}
--- Makefile
+++ Makefile
@@ -6,20 +6,20 @@ setup-gcloud-cli:
 	bash .gcp/setup_gcloud.sh
 # This command should be run as a project owner
 # (run "gcloud auth login" to be an owner account) 
-deploy-resources:
+deploy-resources-jinja:
 	gcloud deployment-manager deployments \
-	create kubeflow-demo-deployment --config .gcp/deployment.yml
+	create kubeflow-demo-deployment --config .gcp/jinja-template/deployment-jinja.yml
 
-delete-resources:
+delete-resources-jinja:
 	gcloud deployment-manager deployments \
 	delete kubeflow-demo-deployment --delete-policy=DELETE
 
 
-deploy-resources-test:
+deploy-resources-python:
 	gcloud deployment-manager deployments \
-	create kubeflow-demo-deployment --config .gcp/bucket.yml
+	create kubeflow-demo-deployment --config .gcp/python-template/deployment-py.yml
 
-delete-resources-test:
+delete-resources-python:
 	gcloud deployment-manager deployments \
 	delete kubeflow-demo-deployment --delete-policy=DELETE
 	
